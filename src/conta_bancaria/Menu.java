768cd210ee104142ba465c798e702b9fb49c365a
Juliana Vitoria <julianaenob@gmail.com>
2026-06-19 14:32:34 -0300

    Atualiza Cores do Menu

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -2,98 +2,93 @@ package conta_bancaria;
 
 import java.util.Scanner;
 
-public class Menu {
+import conta_bancaria.util.Cores;
 
+public class Menu {
 	public static void main(String[] args) {
-		
+
 		Scanner leia = new Scanner(System.in);
-		
+
 		int opcao;
-		
+
 		while (true) {
-			
-			System.out.println("*****************************************************");
+
+			System.out.println(Cores.TEXT_YELLOW + Cores.TEXT_PURPLE
+					+ "*****************************************************");
 			System.out.println("                                                     ");
-			System.out.println("                 BANCO DO BRAZIL COM Z               ");
-			System.out.println("                                                     ");
-			System.out.println("*****************************************************");
-			System.out.println("                                                     ");
-			System.out.println("              1 - Criar Conta                        ");
-			System.out.println("              2 - Listar todas as Contas             ");
-			System.out.println("              3 - Buscar Conta por Numero            ");
-			System.out.println("              4 - Atualizar Dados da Conta           ");
-			System.out.println("              5 - Apagar Conta                       ");
-			System.out.println("              6 - Sacar                             ");
-			System.out.println("              7 - Depositar                         ");
-			System.out.println("              8 - Transferir valores entre Contas    ");
-			System.out.println("              0 - Sair                              ");
+			System.out.println("                BANCO DO BRAZIL COM Z                ");
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
-			System.out.println("Entre com a opção desejada: ");
-			
-			opcao=leia.nextInt();
-			
-			if(opcao==0) {
-				System.out.println("\nBanco do Brazil com Z - O seu Futuro comeca aqui");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Conta                          ");
+			System.out.println("            2 - Listar todas as Contas               ");
+			System.out.println("            3 - Buscar Conta por Numero              ");
+			System.out.println("            4 - Atualizar Dados da Conta             ");
+			System.out.println("            5 - Apagar Conta                         ");
+			System.out.println("            6 - Sacar                                ");
+			System.out.println("            7 - Depositar                            ");
+			System.out.println("            8 - Transferir valores entre Contas      ");
+			System.out.println("            0 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Entre com a opção desejada:                          ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			opcao = leia.nextInt();
+
+			if (opcao == 0) {
+				System.out.println(Cores.TEXT_PURPLE+ "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+				sobre();
 				leia.close();
 				System.exit(0);
-				
-				switch (opcao) {
+			}
 
-				case 1:
-				    System.out.println("Criar Conta\n\n");
-				    break;
+			switch (opcao) {
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
 
-				case 2:
-				    System.out.println("Listar todas as Contas\n\n");
-				    break;
+				break;
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
 
-				case 3:
-				    System.out.println("Consultar dados da Conta - por número\n\n");
-				    break;
+				break;
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
 
-				case 4:
-				    System.out.println("Atualizar dados da Conta\n\n");
-				    break;
+				break;
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
 
-				case 5:
-				    System.out.println("Apagar a Conta\n\n");
-				    break;
+				break;
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
 
-				case 6:
-				    System.out.println("Saque\n\n");
-				    break;
+				break;
+			case 6:
+				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
-				case 7:
-				    System.out.println("Depósito\n\n");
-				    break;
+				break;
+			case 7:
+				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
 
-				case 8:
-				    System.out.println("Transferência entre Contas\n\n");
-				    break;
+				break;
+			case 8:
+				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
 
-				default:
-				    System.out.println("\nOpção Inválida!\n");
-				    break;
-			    }	
-				
+				break;
+			default:
+				System.out.println(Cores.TEXT_PURPLE + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				break;
 			}
 		}
-						
-						
 	}
-				
-				
-				public static void sobre() {
-				        System.out.println("\n*********************************************************");
-				        System.out.println("Projeto Desenvolvido por:");
-				        System.out.println("Juliana Vitoria");
-				        System.out.println("github.com/julianaenob-hub");
-				        System.out.println("*********************************************************");
-			
-			
-		
 
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Juliana Vitoria");
+		System.out.println("github.com/julianaenob-hub");
+		System.out.println("*********************************************************");
 	}
 
 }
